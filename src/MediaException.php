07a58831f46838b4c8f3112726b566c21139d2e8
Igor Tarasov <develop@dicr.org>
2019-01-03 00:45:27 +0500

    initial

---
+++ src/MediaException.php
@@ -0,0 +1,14 @@
+<?php 
+namespace app\lib\media;
+
+use yii\base\Exception;
+
+/**
+ * Ошибка работы с медиа.
+ * 
+ * @author Igor (Dicr) Tarasov <develop@dicr.org>
+ * @version 180628
+ */
+class MediaException extends Exception {
+	
+}
